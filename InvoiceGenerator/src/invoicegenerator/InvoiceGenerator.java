93c408e8dbbf602d9e2a581674299c62fc6bb4e8
teunvdwijst <teunvdwijst@gmail.com>
2018-04-26 14:22:13 +0200

    updated invoice generator form

--- InvoiceGenerator/src/invoicegenerator/InvoiceGenerator.java
+++ InvoiceGenerator/src/invoicegenerator/InvoiceGenerator.java
@@ -5,6 +5,8 @@
  */
 package invoicegenerator;
 
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
@@ -12,8 +14,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -26,54 +28,50 @@ public class InvoiceGenerator extends Application {
     final Button button = new Button("Send");
     final Label notification = new Label();
     final TextField subject = new TextField("");
-    final TextArea text = new TextArea("");
+
+    private void SendInvoice() {
+        // TO DO implement JMS sending of an invoice
+    }
 
     @Override
     public void start(Stage primaryStage) {
+        Pattern pattern = Pattern.compile("\\d+|\\d+\\,\\d{1,2}");
+        TextFormatter formatter = new TextFormatter((UnaryOperator<TextFormatter.Change>) change -> {
+            return pattern.matcher(change.getControlNewText()).matches() ? change : null;
+        });
+
+        subject.setTextFormatter(formatter);
+        button.setOnAction(e -> {
+            SendInvoice();
+        });
         primaryStage.setTitle("InvoiceGenerator");
         Scene scene = new Scene(new Group(), 450, 250);
-        
-        final ComboBox emailComboBox = new ComboBox();
-        emailComboBox.getItems().addAll(
-                "jacob.smith@example.com",
-                "isabella.johnson@example.com",
-                "ethan.williams@example.com",
-                "emma.jones@example.com",
-                "michael.brown@example.com"
+
+        final ComboBox cbPaymentMethod = new ComboBox();
+        cbPaymentMethod.getItems().addAll(
+                "IDeal",
+                "PayPal",
+                "Google Pay"
         );
-        
-        final ComboBox priorityComboBox = new ComboBox();
-        priorityComboBox.getItems().addAll(
-                "Highest",
-                "High",
-                "Normal",
-                "Low",
-                "Lowest"
-        );
-        
-        priorityComboBox.setValue("Normal");
-        
+
         GridPane grid = new GridPane();
         grid.setVgap(4);
         grid.setHgap(10);
         grid.setPadding(new Insets(5, 5, 5, 5));
-        grid.add(new Label("To: "), 0, 0);
-        grid.add(emailComboBox, 1, 0);
-        grid.add(new Label("Priority: "), 2, 0);
-        grid.add(priorityComboBox, 3, 0);
-        grid.add(new Label("Subject: "), 0, 1);
+        grid.add(new Label("Payment Method: "), 0, 0);
+        grid.add(cbPaymentMethod, 1, 0);
+        grid.add(new Label("Price: "), 0, 1);
         grid.add(subject, 1, 1, 3, 1);
-        grid.add(text, 0, 2, 4, 1);
         grid.add(button, 0, 3);
         grid.add(notification, 1, 3, 3, 1);
-        
+
         Group root = (Group) scene.getRoot();
         root.getChildren().add(grid);
         primaryStage.setScene(scene);
-        primaryStage.sizeToScene();
         primaryStage.show();
-        primaryStage.setMinWidth(primaryStage.getWidth());
-        primaryStage.setMinHeight(primaryStage.getHeight());
+        primaryStage.setHeight(150);
+        primaryStage.setWidth(350);
+        primaryStage.resizableProperty().set(false);
     }
 
     /**
